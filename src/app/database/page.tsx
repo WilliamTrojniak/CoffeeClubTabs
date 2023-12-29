8cdaeaaf7e89cf77bb9b11ac2b3a1c572805227b
Will Trojniak <william.trojniak@gmail.com>
2023-12-29 13:00:02 -0500

    migrations for dev database

--- src/app/database/page.tsx
+++ src/app/database/page.tsx
@@ -1,7 +1,10 @@
 import { connect } from "@planetscale/database";
-import { drizzle } from "drizzle-orm/planetscale-serverless";
+// import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
 const DatabasePage = async () => {
+  /*
   const connection = connect({
     host: process.env.DATABASE_HOST,
     username: process.env.DATABASE_USERNAME,
@@ -9,6 +12,15 @@ const DatabasePage = async () => {
   });
 
   const db = drizzle(connection);
+  console.log(db);
+  */
+
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  const db = drizzle(pool);
+
   console.log(db);
 
   return <h1>Database Page</h1>
