9f9d7ae2eeb6cab9ec4b8ce05811fda1bee222ab
Will Trojniak <william.trojniak@gmail.com>
2023-12-29 14:22:41 -0500

    tabs table

---
+++ src/db/tabs.ts
@@ -0,0 +1,28 @@
+import { date, pgEnum, pgTable, serial, text, time, varchar } from "drizzle-orm/pg-core";
+import { users } from "./users";
+
+export const daysOfWeekEnum = pgEnum('days_of_week', 
+  ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
+
+export const tabVerificationMethodsEnum = pgEnum('tab_verification_methods', 
+  ['Specify', 'Email', 'List', 'QR Code', 'Voucher']);
+
+export const tabPaymentMethodsEnum = pgEnum('tab_payment_methods', ['Credit Card', 'University Chartstring']);
+
+export const tabs = pgTable("tabs", {
+  id: serial('id').primaryKey(),
+  ownerId: text('owner_id').references(() => users.id).notNull(),
+  organization: varchar("organization", {length: 255}).notNull(),
+  displayName: varchar("display_name", {length: 255}).notNull(),
+  startDate: date("start_date").notNull(),
+  endDtate: date("end_date").notNull(),
+  dailyStartTime: time('daily_start_time').notNull(),
+  dailyEndTime: time('daily_end_time').notNull(),
+  activeDaysOfWeek: daysOfWeekEnum('active_days_of_week').array(), // TODO this needs to change to allow for multiple
+  verificationMethod: tabVerificationMethodsEnum('verification_method').notNull(),
+  verificationList: varchar('verification_list').array(),
+  paymentMethod: tabPaymentMethodsEnum('payment_method').notNull(), // TODO restrict these values to values available in shop table
+  paymentDetails: varchar('payment_details', {length: 255}), // TODO find out how to make this required based on payment method
+});
+
+
